bc1b6266a058e7106760a976f932141ae2cf11bc
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-20 14:48:17 +0500

    📦 NEW: Priority Queue class header file

---
+++ cPriorityQue/cPriorityQue/cPriorityQue.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "cQueue.h"
+#include <iostream>
+
+using namespace std;
+
+class cPriorityQue:protected cQueue{
+public:
+	cPriorityQue();
+	cPriorityQue(cNode *& ptr);
+	bool isEmpty() const;
+	bool isNotEmpty() const;
+	cPriorityQue & addInPriorityQue(cNode *&ptr);
+	cNode * removeFromPriorityQue();
+	cPriorityQue(const cPriorityQue & src);
+	cPriorityQue operator = (const cPriorityQue & robj);
+	void print() const;
+	~cPriorityQue();
+};
+
