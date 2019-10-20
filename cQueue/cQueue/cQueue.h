15d61d139cd5b4620ef853f75a0bda70a78a4863
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-20 14:38:49 +0500

    📦 NEW: cQueue.h

---
+++ cQueue/cQueue/cQueue.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "cNode.h"
+#include "cStack.h"
+#include <iostream>
+
+using namespace std;
+
+class cQueue :protected cStack
+{
+protected:
+	cNode *tail;
+public:
+	cQueue();
+	cQueue(cNode* &Ptr);
+	cQueue(const cQueue &Source);
+	cQueue& operator=(const cQueue &Source);
+	cQueue& add(cNode* &Ptr);
+	~cQueue();
+	cNode* remove();
+	bool isEmpty() const;
+	bool isNotEmpty() const;
+	void print() const;
+};
