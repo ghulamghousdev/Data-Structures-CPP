49e3a9742fb4d06475d1f345c7cb67cbf4bce81a
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-20 14:28:03 +0500

    📦 NEW: cNode.h

---
+++ Stack/Stack/cNode.h
@@ -0,0 +1,45 @@
+#pragma once
+class cNode
+{
+	/*
+	Variable used to carry value of every node in the STACK
+	*/
+	int data;
+
+public:
+	/*
+	Reference to next cNode Object
+	*/
+	cNode* nextNode;
+
+	/*
+	Default constructor
+	*/
+	cNode();
+
+	/*
+	Parameterized Constuctor
+	*/
+	cNode(int d);
+
+	/*
+	Function to set data in the node of a STACK
+	*/
+	cNode & setData(int number);
+
+	/*
+	Function to get data of a node 
+	*/
+	int getData();
+
+	/*
+	Print Function is used to print all the elements in the STACK
+	*/
+	void print() const;
+
+	/*
+	Default desstructor wouldn't delete the memory allocated on HEAP
+	*/
+	~cNode();
+};
+
