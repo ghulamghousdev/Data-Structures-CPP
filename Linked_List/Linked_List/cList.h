9906d3a9ae4ce071e75ff4c539b3dde3465357b8
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-21 20:48:08 +0500

    📦 NEW: File Handling

--- Linked_List/Linked_List/cList.h
+++ Linked_List/Linked_List/cList.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "cNode.h"
+#include<iostream>
+#include <fstream>
+
+using namespace std;
 
 class cList
 {
@@ -13,25 +17,39 @@ public:
 	//Link List constructors
 	cList();
 	cList(cNode *& ptr);
-	/*cList(ifstream &inFile);
+	//File handling constructors
+	cList(ifstream &inFile);
 	cList(ofstream &outFile);
-	
+	//Function for writing in file
 	void writeListToFile(ofstream &oFile);
-	void readListFromFile(ifstream &inFile);*/
+	//Function for reading from file
+	void readListFromFile(ifstream &inFile);
+	//Functios to check if list is empty or not
 	bool isEmpty();
 	bool isNotEmpty();
+	//Function to insert at the start of the Link List
 	cList & insert(cNode *& ptr);
+	//Function to insert at a specific index
 	cList & insertAt(int index, cNode *& ptr);
+	//Removing the first node from the list
 	cNode* remove();
+	//Function to delete a node from a specific index
 	cNode* removeAt(int index);
+	//Function to swap two nodes
 	cList & swapNodesAt(int index1, int index2);
+	//Function to reverse the list
 	cList & reverse();
+	//Cancatenating one list with another list
 	cList & operator +(const cList & robj);
+	//Printing all the nodes in the cList
 	void print() const;
+	//Function to delete nodes from list
 	void deleteNode(cNode *&ptr);
 	//Copy Constructor of LIST class
 	cList(const cList & src);
+	//Assignment operator overloaded
 	cList& operator =(const cList & robj);
+	//Destructor
 	~cList();
 };
 
