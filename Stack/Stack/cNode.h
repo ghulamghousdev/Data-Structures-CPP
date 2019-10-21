5ed073d0541c4a64e53c63b8abce358594284b9e
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-21 21:20:08 +0500

    📦 NEW: File Handling

--- Stack/Stack/cNode.h
+++ Stack/Stack/cNode.h
@@ -1,45 +1,44 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+using namespace std;
 class cNode
 {
-	/*
-	Variable used to carry value of every node in the STACK
-	*/
+	
+	//Variable used to carry value of every node in the STACK
 	int data;
 
 public:
-	/*
-	Reference to next cNode Object
-	*/
+	
+	//Reference to next cNode Object
 	cNode* nextNode;
-
-	/*
-	Default constructor
-	*/
+	
+	//Default constructor
 	cNode();
 
-	/*
-	Parameterized Constuctor
-	*/
+	//Parameterized Constuctor
 	cNode(int d);
 
-	/*
-	Function to set data in the node of a STACK
-	*/
+	//Class constructor for imput from File
+	cNode(ifstream &inFile); 
+
+	//Member function for input from File
+	void readNodeFromFile(ifstream &inFile);
+
+	cNode(ofstream &oFile);
+	//Memeber function for wrirting output to File
+	void writeNodeToFile(ofstream &oFile);
+	
+	//Function to set data in the node of a STACK
 	cNode & setData(int number);
 
-	/*
-	Function to get data of a node 
-	*/
+	//Function to get data of a node 
 	int getData();
 
-	/*
-	Print Function is used to print all the elements in the STACK
-	*/
+	//Print Function is used to print all the elements in the STACK
 	void print() const;
 
-	/*
-	Default desstructor wouldn't delete the memory allocated on HEAP
-	*/
+	//Default desstructor wouldn't delete the memory allocated on HEAP
 	~cNode();
 };
 
