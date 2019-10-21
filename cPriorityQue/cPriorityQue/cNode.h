2da3762a9b6be62ead28b86d1e45dba269a29a1e
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-21 21:50:42 +0500

    👌 IMPROVE: Commenting

--- cPriorityQue/cPriorityQue/cNode.h
+++ cPriorityQue/cPriorityQue/cNode.h
@@ -8,53 +8,28 @@ class cNode
 	int priority;
 
 public:
-	/*
-	Reference to next cNode Object
-	*/
+	
+	//Reference to next cNode Object
 	cNode* nextNode;
 
-	/*
-	Default constructor
-	*/
+	//Default constructor
 	cNode();
-
-	/*
-	Parameterized Constuctor
-	*/
+	//Parameterized Constuctor
 	cNode(int d);
-
-	/*
-	Function to set data in the node of a STACK
-	*/
+	
+	//Function to set data in the node of a STACK
 	cNode & setData(int number);
-
-	/*
-	Function to get data of a node
-	*/
+	//Function to get data of a node
 	int getData();
-
-	/*
-	Print Function is used to print all the elements in the STACK
-	*/
+	//Print Function is used to print all the elements in the STACK
 	void print() const;
-
-	/*
-	Setting the priority of data
-	*/
+	//Setting the priority of data
 	void setPriority(int prior);
-	/*
-	Getting the priority of data
-	*/
+	//Getting the priority of data
 	int getPriority();
-
-	/*
-	Function to print priority of data
-	*/
+	//Function to print priority of data
 	void printPriority() const;
-
-	/*
-	Default desstructor wouldn't delete the memory allocated on HEAP
-	*/
+	//Default desstructor wouldn't delete the memory allocated on HEAP
 	~cNode();
 };
 
