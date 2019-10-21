dee7d1f14b6840f7ae74e555cae3d8d20c2572a7
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-21 20:49:07 +0500

    📦 NEW: File Handling

--- Linked_List/Linked_List/cNode.h
+++ Linked_List/Linked_List/cNode.h
@@ -1,17 +1,22 @@
 #pragma once
+#include<iostream>
+#include <fstream>
+
+using namespace std;
+
 
 class cNode
 {
+	//Data member to store value
 	int data;
 public:
 	cNode *nextNode;
-
 	cNode();
 	cNode(int d);
-	//cNode(ifstream &inFile);
-	//cNode(ofstream &oFile);
-	//void writeNodetoFile(ofstream &outData);
-	//void readNodefromFile(ifstream &inData);
+	cNode(ifstream &inFile);
+	cNode(ofstream &oFile);
+	void writeNodetoFile(ofstream &outData);
+	void readNodefromFile(ifstream &inData);
 	cNode & setData(int d);
 	int getData();
 	void print() const;
