4e9a35a49fff97930e5b4e97a87450f158113048
ghulamghousdev <itsghulamghous@gmail.com>
2019-10-25 01:23:07 +0500

    📦 NEW: first commit

---
+++ cCircularList/cCircularList/cNode.h
@@ -0,0 +1,29 @@
+#pragma once
+/*
+Header file for cNode class
+*/
+#include<iostream>
+#include <iostream>
+using namespace std;
+#include <fstream>
+
+using namespace std;
+
+
+class cNode
+{
+	//Data member to store value
+	int data;
+public:
+	cNode *nextNode;
+	cNode();
+	cNode(int d);
+	cNode(ifstream &inFile);
+	cNode(ofstream &oFile);
+	void writeNodetoFile(ofstream &outData);
+	void readNodefromFile(ifstream &inData);
+	cNode & setData(int d);
+	int getData();
+	void print() const;
+};
+
