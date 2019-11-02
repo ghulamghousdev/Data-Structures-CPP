932871291369ce763b67423a4a6a5b269b9dddd2
ghulamghousdev <itsghulamghous@gmail.com>
2019-11-02 23:06:38 +0500

    📦 NEW: flip,reverse,sorting functions

--- cCircularList/cCircularList/driver.cpp
+++ cCircularList/cCircularList/driver.cpp
@@ -9,19 +9,23 @@ int main() {
 
 	cCircularList list;
 	cNode* n1 = new cNode(10);
-	cNode* n2 = new cNode(20);
-	cNode* n3 = new cNode(30);
+	cNode* n2 = new cNode(120);
+	cNode* n3 = new cNode(302);
 	cNode* n4 = new cNode(40);
-	cNode* n5 = new cNode(50);
-	cNode* n6 = new cNode(60);
-	cNode* n7 = new cNode(70);
+	cNode* n5 = new cNode(222);
+	cNode* n6 = new cNode(42);
+	cNode* n7 = new cNode(701);
 
-	list.insert(n1).insert(n2).insert(n3);
+	list.insert(n1).insert(n2).insert(n3).insert(n4).insert(n5).insert(n6).insert(n7);
+	//list.print();
+	//list.swapNodesAt(3, 4);
 	list.print();
-	list.insertAt(5, n4);
+	list.sorting();
 	list.print();
 	list.reverse();
 	list.print();
-
+	list.flip();
+	list.print();
+	cout << endl << endl;
 	system("pause");
 }
